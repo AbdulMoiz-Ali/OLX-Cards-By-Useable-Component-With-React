e9bf2565dde7a76c344261facfa0d1c8923e14a0
Moiz Ali <abdulmoizali321@gmail.com>
2024-09-06 11:14:41 +0500

    Ooop

--- src/component/footer/footer.jsx
+++ src/component/footer/footer.jsx
@@ -1,9 +1,9 @@
 function Footer() {
     return (
         <>
-                <div style={{backgroundColor:"black"}} class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-                    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                    <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© Devalop By <span style={{color:"blue"}}>MOiZ AlI</span></span>
+                <div style={{backgroundColor:"black"}} className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+                    <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+                    <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© Devalop By <span style={{color:"blue"}}>MOiZ AlI</span></span>
                 </div>
         </>
     )
